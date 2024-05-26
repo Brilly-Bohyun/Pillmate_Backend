156544037fb8bc5b65f992e60d06452e783f4c0d
Brilly-Bohyun <bohyun564@swu.ac.kr>
2024-05-27 00:54:51 +0900

    🚩Alarm Feature

--- src/main/java/pillmate/backend/repository/MedicineRepository.java
+++ src/main/java/pillmate/backend/repository/MedicineRepository.java
@@ -3,5 +3,8 @@ package pillmate.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pillmate.backend.entity.Medicine;
 
+import java.util.Optional;
+
 public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+    Optional<Medicine> findByName(String name);
 }
