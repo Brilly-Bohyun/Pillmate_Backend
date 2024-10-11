c18ba5651b26978ecd5fd0e9bcbe5aae393f8662
Brilly-Bohyun <bohyun564@swu.ac.kr>
2024-10-11 21:16:43 +0900

    :wrench: Change Token Expiration Time

--- src/main/java/pillmate/backend/common/util/JwtTokenConst.java
+++ src/main/java/pillmate/backend/common/util/JwtTokenConst.java
@@ -2,8 +2,8 @@ package pillmate.backend.common.util;
 
 public class JwtTokenConst {
     // Token
-    public static final Long ACCESS_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24 * 30;        // ACCESS 토큰 만료 시간 (30일)
-    public static final Long REFRESH_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24 * 31;   // REFRESH 토큰 만료 시간 (31일)
+    public static final Long ACCESS_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 6;        // ACCESS 토큰 만료 시간 (6시간)
+    public static final Long REFRESH_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24;   // REFRESH 토큰 만료 시간 (24시간)
     public static final Long REISSUE_EXPIRE_TIME = 1000L * 60 * 60 * 3;         // Reissue 만료 시간 (3시간)
 
     // header
